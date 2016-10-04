4aa7668466d5082ebb6c32caf76833d3b184f851
deanturpin <git@lvbtn.uk>
2016-10-04 21:16:55 +0100

    simplify debug

--- histogram.cpp
+++ histogram.cpp
@@ -7,16 +7,13 @@ int main() {
 
 	using namespace std;
 
-	cout << "Length of sequence " << seq::uence.size() << endl;
-
 	// Create histogram
 	map<unsigned int, unsigned int> histogram;
-
 	for (const auto &s : seq::uence)
 		++histogram[s / 2000];
 
+	// Print histogram
 	cout << "Size of histogram " << histogram.size() << endl;
-
 	for (const auto &h : histogram)
 		cout << h.first << '\t' << string(h.second, '-') << '|' << endl;
 
