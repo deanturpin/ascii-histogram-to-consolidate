f237e7a077261e412d28a3d997bc5c90fc7fc529
deanturpin <git@lvbtn.uk>
2016-10-05 18:45:33 +0100

    rename

--- histogram.cpp
+++ histogram.cpp
@@ -10,12 +10,12 @@ int main() {
 	// Create histogram
 	map<unsigned int, unsigned int> histogram;
 	for (const auto &s : seq::uence)
-		++histogram[s / 2000];
+		++histogram[s / 1200];
 
 	// Print histogram
 	cout << "Size of histogram " << histogram.size() << endl;
 	for (const auto &h : histogram)
-		cout << h.first << '\t' << string(h.second, '-') << '|' << endl;
+		cout << h.first << '\t' << string(h.second * 8, '-') << '|' << endl;
 
 	return 0;
 }
