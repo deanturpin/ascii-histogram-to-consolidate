47045d6c128294138a47693d3394e1fd0bcfa055
deanturpin <git@lvbtn.uk>
2016-10-05 22:01:19 +0100

    use for_each where possible

--- histogram_with_fade.cpp
+++ histogram_with_fade.cpp
@@ -1,6 +1,7 @@
+#include <algorithm>
 #include <iostream>
-#include <map>
 #include <unistd.h>
+#include <map>
 
 #include "sequence.h"
 
@@ -13,17 +14,17 @@ int main() {
 	for (const auto &s : seq::uence)
 		++histogram[s / 1200];
 
-	// Print histogram
-	cout << "Size of histogram " << histogram.size() << endl;
-	for (const auto &h : histogram)
-		cout << h.first << '\t' << string(h.second * 8, '-') << "| " << h.first << endl;
-
 	// Fade each bin and delete when empty
 	while (histogram.size()) {
 
-		// Decrement
-		// for_each
+		// Print histogram
+		for_each (histogram.cbegin(), histogram.cend(), [] (auto &h) {
+			cout << h.first << '\t' << string(h.second * 8, '-') << "| " << h.first << endl;
+		});
+
+		// Decrement each bin
 		for (auto &h : histogram) {
+
 			--h.second;
 
 			if (0 == h.second)
@@ -33,9 +34,6 @@ int main() {
 		// Jump to top of screen
 		cout << "\x1b[A";
 		cout << "\033[2J";
-
-		for (const auto &h : histogram)
-			cout << h.first << '\t' << string(h.second * 8, '-') << "| " << h.first << endl;
 
 		usleep(500000);
 	}
